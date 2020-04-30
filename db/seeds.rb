a0a07a152c102784c64d56d8f3fbc33dff68a622
TimotheRuffino <timothe.ruffino@gmail.com>
2020-04-30 23:08:27 +0200

    first-commit

---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+City.destroy_all
+User.destroy_all
+
+
+10.times do
+  city = City.create!(
+    name: Faker::Address.city,
+    zip_code: Faker::Address.zip_code
+  )
+end
+
+10.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.sentence(word_count: 10),
+    email: Faker::Internet.email,
+    age: rand(18..28)
+  )
+end
